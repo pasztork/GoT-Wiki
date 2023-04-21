239d8571b06e21ce19ad816bb79b17ce387b594e
Pásztor Kristóf <pasztor.kristof.kp@gmail.com>
2023-04-21 12:53:35 +0200

    refactor: change loading order

--- ViewModels/DetailsViewModels/HouseDetailsPageViewModel.cs
+++ ViewModels/DetailsViewModels/HouseDetailsPageViewModel.cs
@@ -36,8 +36,8 @@ namespace GoT_Wiki.ViewModels
 
         protected override async Task OnLoad()
         {
-            await FetchCharacters();
             await FetchHouses();
+            await FetchCharacters();
         }
 
         private async Task FetchCharacters()
